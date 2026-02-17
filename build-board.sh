23e2a6a297321a57c1fd7a8065b9a76b73fccea0
Vladlen Chekunov <chekunov@national.shitposting.agency>
2026-02-18 00:35:03 +0300

    Fix build folder, add all pieces into the build script, implement infantry piece.

--- build-board.sh
+++ build-board.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsvg-convert -f pdf -o ./builds/board_A3.pdf ./src/board/board_A3.svg
+rsvg-convert -f pdf -o ./build/board_A3.pdf ./src/board/board_A3.svg
--- build-pieces.sh
+++ build-pieces.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-# FIXME: Implement
+# FIXME: Implement and uncomment all pieces below
+
+# Units
+solvespace-cli export-mesh --output ./build/infantry.stl ./src/pieces/units/infantry.slvs
+# solvespace-cli export-mesh --output ./build/foot_artillery.stl ./src/pieces/units/foot_artillery.slvs
+# solvespace-cli export-mesh --output ./build/foot_transmission.stl ./src/pieces/units/foot_transmission.slvs
+# solvespace-cli export-mesh --output ./build/mounted_artillery.stl ./src/pieces/units/mounted_artillery.slvs
+# solvespace-cli export-mesh --output ./build/cavalry.stl ./src/pieces/units/cavalry.slvs
+# solvespace-cli export-mesh --output ./build/mounted_transmission.stl ./src/pieces/units/mounted_transmission.slvs
+
+# Strongholds
+# solvespace-cli export-mesh --output ./build/fort_south.stl ./src/pieces/strongholds/fort_south.slvs
+# solvespace-cli export-mesh --output ./build/fort_north.stl ./src/pieces/strongholds/fort_north.slvs
+# solvespace-cli export-mesh --output ./build/arsenal.stl ./src/pieces/strongholds/arsenal.slvs
+# solvespace-cli export-mesh --output ./build/mountain.stl ./src/pieces/strongholds/mountain.slvs
+# solvespace-cli export-mesh --output ./build/mountain_pass.stl ./src/pieces/strongholds/mountain_pass.slvs
+
