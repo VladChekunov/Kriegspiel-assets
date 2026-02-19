542b5c5e6aabe7d74e6fb8d9258305fe41defac1
Vladlen Chekunov <chekunov@national.shitposting.agency>
2026-02-19 09:31:14 +0300

    Add mounted artillery

--- build-pieces.sh
+++ build-pieces.sh
@@ -6,7 +6,7 @@
 solvespace-cli export-mesh -t 0.05 --output ./build/infantry.stl ./src/pieces/units/infantry.slvs
 solvespace-cli export-mesh -t 0.05 --output ./build/foot_artillery.stl ./src/pieces/units/foot_artillery.slvs
 solvespace-cli export-mesh -t 0.05 --output ./build/foot_transmission.stl ./src/pieces/units/foot_transmission.slvs
-# solvespace-cli export-mesh -t 0.05 --output ./build/mounted_artillery.stl ./src/pieces/units/mounted_artillery.slvs
+solvespace-cli export-mesh -t 0.05 --output ./build/mounted_artillery.stl ./src/pieces/units/mounted_artillery.slvs
 # solvespace-cli export-mesh -t 0.05 --output ./build/cavalry.stl ./src/pieces/units/cavalry.slvs
 # solvespace-cli export-mesh -t 0.05 --output ./build/mounted_transmission.stl ./src/pieces/units/mounted_transmission.slvs
 
