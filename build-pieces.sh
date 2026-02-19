9b7dc07b1254d168ccd439fcf12fb0665226703c
Vladlen Chekunov <chekunov@national.shitposting.agency>
2026-02-19 08:53:42 +0300

    Add foot artillery, foot transmission and arsenal models.

--- build-pieces.sh
+++ build-pieces.sh
@@ -5,7 +5,7 @@
 # Units
 solvespace-cli export-mesh -t 0.05 --output ./build/infantry.stl ./src/pieces/units/infantry.slvs
 solvespace-cli export-mesh -t 0.05 --output ./build/foot_artillery.stl ./src/pieces/units/foot_artillery.slvs
-# solvespace-cli export-mesh -t 0.05 --output ./build/foot_transmission.stl ./src/pieces/units/foot_transmission.slvs
+solvespace-cli export-mesh -t 0.05 --output ./build/foot_transmission.stl ./src/pieces/units/foot_transmission.slvs
 # solvespace-cli export-mesh -t 0.05 --output ./build/mounted_artillery.stl ./src/pieces/units/mounted_artillery.slvs
 # solvespace-cli export-mesh -t 0.05 --output ./build/cavalry.stl ./src/pieces/units/cavalry.slvs
 # solvespace-cli export-mesh -t 0.05 --output ./build/mounted_transmission.stl ./src/pieces/units/mounted_transmission.slvs
@@ -13,7 +13,7 @@ solvespace-cli export-mesh -t 0.05 --output ./build/foot_artillery.stl ./src/pie
 # Strongholds
 # solvespace-cli export-mesh -t 0.05 --output ./build/fort_south.stl ./src/pieces/strongholds/fort_south.slvs
 # solvespace-cli export-mesh -t 0.05 --output ./build/fort_north.stl ./src/pieces/strongholds/fort_north.slvs
-# solvespace-cli export-mesh -t 0.05 --output ./build/arsenal.stl ./src/pieces/strongholds/arsenal.slvs
+solvespace-cli export-mesh -t 0.05 --output ./build/arsenal.stl ./src/pieces/strongholds/arsenal.slvs
 # solvespace-cli export-mesh -t 0.05 --output ./build/mountain.stl ./src/pieces/strongholds/mountain.slvs
 # solvespace-cli export-mesh -t 0.05 --output ./build/mountain_pass.stl ./src/pieces/strongholds/mountain_pass.slvs
 
