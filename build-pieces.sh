2e3e869801a615b9be52346ef96f6d5b7846617e
Vladlen Chekunov <chekunov@national.shitposting.agency>
2026-02-19 22:53:22 +0300

    Add cavalry, mounted artillery and mounted transmission

--- build-pieces.sh
+++ build-pieces.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# FIXME: Implement and uncomment all pieces below
-
 # Units
 solvespace-cli export-mesh -t 0.05 --output ./build/infantry.stl ./src/pieces/units/infantry.slvs
 solvespace-cli export-mesh -t 0.05 --output ./build/foot_artillery.stl ./src/pieces/units/foot_artillery.slvs
 solvespace-cli export-mesh -t 0.05 --output ./build/foot_transmission.stl ./src/pieces/units/foot_transmission.slvs
 solvespace-cli export-mesh -t 0.05 --output ./build/mounted_artillery.stl ./src/pieces/units/mounted_artillery.slvs
-# solvespace-cli export-mesh -t 0.05 --output ./build/cavalry.stl ./src/pieces/units/cavalry.slvs
-# solvespace-cli export-mesh -t 0.05 --output ./build/mounted_transmission.stl ./src/pieces/units/mounted_transmission.slvs
+solvespace-cli export-mesh -t 0.05 --output ./build/cavalry.stl ./src/pieces/units/cavalry.slvs
+solvespace-cli export-mesh -t 0.05 --output ./build/mounted_transmission.stl ./src/pieces/units/mounted_transmission.slvs
+
+# FIXME: Implement and uncomment all strongholds below
 
 # Strongholds
 # solvespace-cli export-mesh -t 0.05 --output ./build/fort_south.stl ./src/pieces/strongholds/fort_south.slvs
