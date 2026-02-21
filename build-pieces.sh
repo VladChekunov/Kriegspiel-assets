3d64f8cb386062bb62e34e2f18ec5cdfeea0b808
Vladlen Chekunov <vladlenchekunov@yandex.com>
2026-02-21 21:31:23 +0300

    Add mountain. Get pieces builder script done.

--- build-pieces.sh
+++ build-pieces.sh
@@ -8,12 +8,9 @@ solvespace-cli export-mesh -t 0.05 --output ./build/mounted_artillery.stl ./src/
 solvespace-cli export-mesh -t 0.05 --output ./build/cavalry.stl ./src/pieces/units/cavalry.slvs
 solvespace-cli export-mesh -t 0.05 --output ./build/mounted_transmission.stl ./src/pieces/units/mounted_transmission.slvs
 
-# FIXME: Implement and uncomment all strongholds below
-
 # Strongholds
 solvespace-cli export-mesh -t 0.05 --output ./build/fort_south.stl ./src/pieces/strongholds/fort_south.slvs
 solvespace-cli export-mesh -t 0.05 --output ./build/fort_north.stl ./src/pieces/strongholds/fort_north.slvs
 solvespace-cli export-mesh -t 0.05 --output ./build/arsenal.stl ./src/pieces/strongholds/arsenal.slvs
-# solvespace-cli export-mesh -t 0.05 --output ./build/mountain.stl ./src/pieces/strongholds/mountain.slvs
-# solvespace-cli export-mesh -t 0.05 --output ./build/mountain_pass.stl ./src/pieces/strongholds/mountain_pass.slvs
+solvespace-cli export-mesh -t 0.05 --output ./build/mountain.stl ./src/pieces/strongholds/mountain.slvs
 
